9a56d0f316d69ba812ad34f5f5c01faa505fcec8
Emi <dealuminis@gmail.com>
2017-06-21 23:43:19 -0700

    Add menu bar
    Signed-off-by: Emi <dealuminis@gmail.com>

--- src/main/java/emi/mtg/deckbuilder/view/MainWindow.java
+++ src/main/java/emi/mtg/deckbuilder/view/MainWindow.java
@@ -2,10 +2,12 @@ package emi.mtg.deckbuilder.view;
 
 import com.sun.javafx.collections.ObservableListWrapper;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
+import javafx.scene.CacheHint;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.transform.Scale;
@@ -37,41 +39,70 @@ public class MainWindow extends Application {
 
 		StackPane root = new StackPane();
 
+		MenuBar menu = new MenuBar();
+
+		Menu file = new Menu("File");
+		MenuItem unloadAll = new MenuItem("Unload All Images");
+		MenuItem loadAll = new MenuItem("Load All Images");
+		MenuItem gc = new MenuItem("Collect Garbage");
+		file.getItems().addAll(unloadAll, loadAll, gc);
+
+		menu.getMenus().addAll(file);
+
 		BorderPane layout = new BorderPane();
 
 		Slider zoom = new Slider(0.25, 1.5, 0.5);
-		layout.setTop(zoom);
+		layout.setTop(menu);
+
+		BorderPane pilesPane = new BorderPane();
 
 		ScrollPane scroll = new ScrollPane();
+		scroll.setPannable(true);
 
 		List<CardInstance> cards = new ArrayList<>();
 		cs.cards().stream()
 				.filter(c -> "AVR".equals(c.set().code()))
-				.filter(c -> c.manaCost() != null && c.manaCost().convertedCost() <= 4)
+//				.filter(c -> c.manaCost() != null && c.manaCost().convertedCost() <= 4)
 //				.filter(c -> CardRarity.Rare.equals(c.rarity()) || CardRarity.MythicRare.equals(c.rarity()))
+//				.filter(c -> CardRarity.MythicRare.equals(c.rarity()))
 //				.filter(c -> c.color().size() > 1)
 				.map(CardInstance::new)
 				.forEach(cards::add);
 		ObservableList<CardInstance> model = new ObservableListWrapper<>(cards);
 
-		NewPilesView pilesView = new NewPilesView(is, model, NewPilesView.COLOR_SORT.thenComparing(NewPilesView.NAME_SORT), ci -> ci.card.rarity().toString(), (r1, r2) -> CardRarity.forString(r1).compareTo(CardRarity.forString(r2)));
+		NewPilesView pilesView = new NewPilesView(is, model);
+		pilesView.setCache(true);
+
+		unloadAll.setOnAction(ae -> pilesView.unloadAll());
+		loadAll.setOnAction(ae -> pilesView.loadAll());
+		gc.setOnAction(ae -> {
+			System.gc();
+			System.gc();
+		});
 
 		final Scale zoomXform = new Scale(0.5, 0.5);
 
-		zoom.valueProperty().addListener(change -> {
-			zoomXform.setX(zoom.getValue());
-			zoomXform.setY(zoom.getValue());
-			scroll.requestLayout();
-		});
+		zoomXform.xProperty().bind(zoom.valueProperty());
+		zoomXform.yProperty().bind(zoom.valueProperty());
 
 		pilesView.getTransforms().add(zoomXform);
 
-		scroll.setContent(pilesView);
-		layout.setCenter(scroll);
+		scroll.setContent(new Group(pilesView));
+		pilesPane.setTop(zoom);
+		pilesPane.setCenter(scroll);
+
+		layout.setCenter(pilesPane);
 
 		root.getChildren().add(layout);
 
-		stage.setScene(new Scene(root, 512, 512));
+		stage.setScene(new Scene(root));
+/*
+		root.getChildren().add(new CardInstanceView(new CardInstance(cs.cards().stream()
+				.filter(c -> "AVR".equals(c.set().code()))
+				.filter(c -> "Avacyn, Angel of Hope".equals(c.name()))
+				.findAny().get()), new RenderedImageSource()));
+
+		stage.setScene(new Scene(root));*/
 		stage.setMaximized(true);
 		stage.show();
 	}
