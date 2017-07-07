a278594619a01f39c9e0aa328946b67ebb6448b0
Emi <dealuminis@gmail.com>
2017-07-06 23:05:03 -0700

    Separated lands and nonlands in CMC grouping
    Signed-off-by: Emi <dealuminis@gmail.com>

--- src/main/java/emi/mtg/deckbuilder/view/groupings/ConvertedManaCost.java
+++ src/main/java/emi/mtg/deckbuilder/view/groupings/ConvertedManaCost.java
@@ -1,6 +1,7 @@
 package emi.mtg.deckbuilder.view.groupings;
 
 import emi.lib.Service;
+import emi.lib.mtg.characteristic.CardType;
 import emi.lib.mtg.characteristic.ManaCost;
 import emi.mtg.deckbuilder.model.CardInstance;
 import emi.mtg.deckbuilder.view.CardView;
@@ -8,7 +9,7 @@ import emi.mtg.deckbuilder.view.CardView;
 @Service.Provider(CardView.Grouping.class)
 @Service.Property.String(name="name", value="CMC")
 public class ConvertedManaCost implements CardView.Grouping {
-	private static final String[] GROUPS = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "X" };
+	private static final String[] GROUPS = { "Land", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "X" };
 
 	@Override
 	public String[] groups() {
@@ -17,6 +18,10 @@ public class ConvertedManaCost implements CardView.Grouping {
 
 	@Override
 	public String extract(CardInstance ci) {
+		if (ci.card().front().type().cardTypes().contains(CardType.Land)) {
+			return "Land";
+		}
+
 		ManaCost mc = ci.card().front().manaCost();
 		return mc.varies() ? "X" : Integer.toString(mc.convertedCost());
 	}
