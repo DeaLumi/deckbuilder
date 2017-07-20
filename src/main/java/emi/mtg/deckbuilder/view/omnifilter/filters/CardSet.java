f5604b2fff2ea102bb21c42dc1b386cf367e6963
Emi <dealuminis@gmail.com>
2017-07-19 17:47:33 -0700

    Add card set subfilter
    Signed-off-by: Emi <dealuminis@gmail.com>

---
+++ src/main/java/emi/mtg/deckbuilder/view/omnifilter/filters/CardSet.java
@@ -0,0 +1,37 @@
+package emi.mtg.deckbuilder.view.omnifilter.filters;
+
+import emi.lib.Service;
+import emi.mtg.deckbuilder.model.CardInstance;
+import emi.mtg.deckbuilder.view.omnifilter.Omnifilter;
+
+@Service.Provider(Omnifilter.Subfilter.class)
+@Service.Property.String(name="key", value="set")
+@Service.Property.String(name="shorthand", value="s")
+public class CardSet implements Omnifilter.Subfilter {
+	private final Omnifilter.Operator operator;
+	private final String value;
+
+	public CardSet(Omnifilter.Operator operator, String value) {
+		this.operator = operator;
+		this.value = value.toLowerCase();
+	}
+
+	@Override
+	public boolean test(CardInstance ci) {
+		switch (operator) {
+			case DIRECT:
+			case EQUALS:
+				return ci.card().set().name().toLowerCase().equals(value) || ci.card().set().code().toLowerCase().equals(value);
+			case NOT_EQUALS:
+				return !ci.card().set().name().toLowerCase().equals(value) && !ci.card().set().code().toLowerCase().equals(value);
+			case LESS_THAN:
+			case GREATER_THAN:
+			case LESS_OR_EQUALS:
+			case GREATER_OR_EQUALS:
+				return true;
+			default:
+				assert false;
+				return false;
+		}
+	}
+}
