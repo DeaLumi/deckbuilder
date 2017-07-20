eb9e9881840039c5f044374dcdfcd0bdef2988d4
Emi <dealuminis@gmail.com>
2017-07-19 17:58:55 -0700

    Add card rarity subfilter
    Signed-off-by: Emi <dealuminis@gmail.com>

---
+++ src/main/java/emi/mtg/deckbuilder/view/omnifilter/filters/Rarity.java
@@ -0,0 +1,48 @@
+package emi.mtg.deckbuilder.view.omnifilter.filters;
+
+import emi.lib.Service;
+import emi.lib.mtg.characteristic.CardRarity;
+import emi.mtg.deckbuilder.model.CardInstance;
+import emi.mtg.deckbuilder.view.omnifilter.Omnifilter;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@Service.Provider(Omnifilter.Subfilter.class)
+@Service.Property.String(name="key", value="rarity")
+@Service.Property.String(name="shorthand", value="r")
+public class Rarity implements Omnifilter.Subfilter {
+	private final Omnifilter.Operator operator;
+	private final CardRarity value;
+
+	public Rarity(Omnifilter.Operator operator, String value) {
+		this.operator = operator;
+
+		this.value = Arrays.stream(CardRarity.values())
+				.filter(r -> r.toString().startsWith(value.toLowerCase()))
+				.findAny()
+				.orElseThrow(() -> new IllegalArgumentException("Couldn't find a card rarity for " + value));
+	}
+
+	@Override
+	public boolean test(CardInstance cardInstance) {
+		switch (this.operator) {
+			case DIRECT:
+			case EQUALS:
+				return cardInstance.card().rarity() == this.value;
+			case NOT_EQUALS:
+				return cardInstance.card().rarity() != this.value;
+			case GREATER_OR_EQUALS:
+				return cardInstance.card().rarity().ordinal() >= this.value.ordinal();
+			case GREATER_THAN:
+				return cardInstance.card().rarity().ordinal() > this.value.ordinal();
+			case LESS_OR_EQUALS:
+				return cardInstance.card().rarity().ordinal() <= this.value.ordinal();
+			case LESS_THAN:
+				return cardInstance.card().rarity().ordinal() < this.value.ordinal();
+			default:
+				assert false;
+				return false;
+		}
+	}
+}
