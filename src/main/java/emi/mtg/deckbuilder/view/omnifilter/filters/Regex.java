0ff179b80d80c5c20d6cc55ec38a69cf5c35d24f
Emi <dealuminis@gmail.com>
2017-08-29 08:10:41 -0700

    Add regex filter.
    I don't have the flexibility I wanted out of Scryfall's regex filter (specifically generic character classes).
    So I added my own to my own program...
    
    Signed-off-by: Emi <dealuminis@gmail.com>

---
+++ src/main/java/emi/mtg/deckbuilder/view/omnifilter/filters/Regex.java
@@ -0,0 +1,35 @@
+package emi.mtg.deckbuilder.view.omnifilter.filters;
+
+import emi.lib.Service;
+import emi.lib.mtg.Card;
+import emi.mtg.deckbuilder.view.omnifilter.Omnifilter;
+
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+@Service.Provider(Omnifilter.Subfilter.class)
+@Service.Property.String(name="key", value="regex")
+@Service.Property.String(name="shorthand", value="re")
+public class Regex implements Omnifilter.FaceFilter {
+	private Pattern pattern = null;
+
+	public Regex(Omnifilter.Operator operator, String value) {
+		try {
+			if (operator == Omnifilter.Operator.DIRECT) {
+				this.pattern = Pattern.compile(value);
+			}
+		} catch (PatternSyntaxException pse) {
+			/* do nothing */
+			pse.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean testFace(Card.Face face) {
+		if (this.pattern != null) {
+			return this.pattern.matcher(face.rules()).find();
+		} else {
+			return true;
+		}
+	}
+}
