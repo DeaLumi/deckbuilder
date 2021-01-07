9c7698109853e9d15f20b541d0468411d99a92e6
Emi <dealuminis@gmail.com>
2021-01-07 12:46:59 -0800

    Rewrite typeadapters
    This is *definitely* gonna break something, right...?
    
    Signed-off-by: Emi <dealuminis@gmail.com>

--- src/main/java/emi/mtg/deckbuilder/controller/TypeAdapters.java
+++ src/main/java/emi/mtg/deckbuilder/controller/TypeAdapters.java
@@ -8,8 +8,8 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import emi.lib.mtg.Card;
-import emi.lib.mtg.DataSource;
 import emi.lib.mtg.game.Format;
+import emi.mtg.deckbuilder.controller.serdes.impl.NameOnlyImporter;
 import emi.mtg.deckbuilder.view.components.CardView;
 import emi.mtg.deckbuilder.view.groupings.ConvertedManaCost;
 import javafx.beans.property.Property;
@@ -23,166 +23,87 @@ import java.lang.reflect.Type;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
+import java.util.function.Function;
 
 public class TypeAdapters {
-	public static TypeAdapter<Format> createFormatAdapter() {
-		return new TypeAdapter<Format>() {
-			@Override
-			public void write(JsonWriter out, Format value) throws IOException {
-				if (value == null) {
-					out.nullValue();
-				} else {
-					out.value(value.name());
-				}
+	private static class StringTypeAdapter<T> extends TypeAdapter<T> {
+		protected final Function<T, String> toString;
+		protected final Function<String, T> fromString;
+
+		public StringTypeAdapter(Function<T, String> toString, Function<String, T> fromString) {
+			this.toString = toString;
+			this.fromString = fromString;
+		}
+
+		@Override
+		public void write(JsonWriter out, T value) throws IOException {
+			if (value == null) {
+				out.nullValue();
+			} else {
+				out.value(toString.apply(value));
+			}
+		}
+
+		@Override
+		public T read(JsonReader in) throws IOException {
+			String v;
+			switch (in.peek()) {
+				case STRING:
+					v = in.nextString();
+					break;
+				case NAME:
+					v = in.nextName();
+					break;
+				default:
+					throw new IOException("JSON file error: Expected string or name, got " + in.peek().name());
 			}
 
-			@Override
-			public Format read(JsonReader in) throws IOException {
-				String v;
-				switch (in.peek()) {
-					case STRING:
-						v = in.nextString();
-						break;
-					case NAME:
-						v = in.nextName();
-						break;
-					default:
-						return null;
-				}
-				if ("EDH".equals(v)) v = "Commander"; // Quick hack for continuity.
-				try {
-					return Format.valueOf(v);
-				} catch (IllegalArgumentException iae) {
-					return Format.Freeform;
-				}
+			return fromString.apply(v);
+		}
+	}
+
+	public static TypeAdapter<Format> createFormatAdapter() {
+		return new StringTypeAdapter<>(Format::name, s -> {
+			if ("EDH".equals(s)) s = "Commander";
+			try {
+				return Format.valueOf(s);
+			} catch (IllegalArgumentException iae) {
+				return Format.Freeform;
 			}
-		};
+		});
 	}
 
 	public static TypeAdapter<CardView.Grouping> createCardViewGroupingAdapter() {
-		return new TypeAdapter<CardView.Grouping>() {
-			@Override
-			public void write(JsonWriter out, CardView.Grouping value) throws IOException {
-				if (value == null) {
-					out.nullValue();
-				} else {
-					out.value(value.name());
-				}
-			}
-
-			@Override
-			public CardView.Grouping read(JsonReader in) throws IOException {
-				String v;
-				switch (in.peek()) {
-					case NAME:
-						v = in.nextName();
-						break;
-					case STRING:
-						v = in.nextString();
-						break;
-					default:
-						return null;
-				}
-
-				return CardView.GROUPINGS.stream()
-						.filter(g -> g.name().equals(v))
-						.findAny()
-						.orElseGet(() -> {
-							System.err.println(String.format("Couldn't find grouping factory %s! Did a plugin go away? Defaulting to CMC...", v));
-							return ConvertedManaCost.INSTANCE;
-						});
-			}
-		};
+		return new StringTypeAdapter<>(CardView.Grouping::name, s -> CardView.GROUPINGS.stream()
+				.filter(g -> g.name().equals(s))
+				.findAny()
+				.orElseGet(() -> {
+					System.err.println(String.format("Couldn't find grouping factory %s! Did a plugin go away? Defaulting to CMC...", s));
+					return ConvertedManaCost.INSTANCE;
+				}));
 	}
 
-	public static TypeAdapter<Card.Printing> createCardPrintingAdapter(DataSource data) {
-		return new TypeAdapter<Card.Printing>() {
-			@Override
-			public void write(JsonWriter out, Card.Printing value) throws IOException {
-				out.value(value.id().toString());
-			}
+	public static TypeAdapter<Card> createCardAdapter() {
+		return new StringTypeAdapter<>(Card::fullName, NameOnlyImporter::findCard);
+	}
 
-			@Override
-			public Card.Printing read(JsonReader in) throws IOException {
-				String v;
-				switch (in.peek()) {
-					case STRING:
-						v = in.nextString();
-						break;
-					case NAME:
-						v = in.nextName();
-						break;
-					default:
-						return null;
-				}
-				return data.printing(UUID.fromString(v));
-			}
-		};
+	public static TypeAdapter<Card.Printing> createCardPrintingAdapter() {
+		return new StringTypeAdapter<>(p -> p.id().toString(), i -> Context.get().data.printing(UUID.fromString(i)));
 	}
 
 	public static TypeAdapter<Path> createPathTypeAdapter() {
-		return new TypeAdapter<Path>() {
-			@Override
-			public void write(JsonWriter jsonWriter, Path path) throws IOException {
-				if (path == null) {
-					jsonWriter.nullValue();
-				} else {
-					jsonWriter.value(path.toString());
-				}
-			}
-
-			@Override
-			public Path read(JsonReader jsonReader) throws IOException {
-				switch (jsonReader.peek()) {
-					case NULL:
-						return null;
-					case STRING:
-						return Paths.get(jsonReader.nextString());
-					case NAME:
-						return Paths.get(jsonReader.nextName());
-					default:
-						assert false;
-						return null;
-				}
-			}
-		};
+		return new StringTypeAdapter<>(Path::toString, Paths::get);
 	}
 
 	public static TypeAdapter<CardView.ActiveSorting> createActiveSortingTypeAdapter() {
-		return new TypeAdapter<CardView.ActiveSorting>() {
-			@Override
-			public void write(JsonWriter out, CardView.ActiveSorting value) throws IOException {
-				out.value(String.format("%s%s", value.descending.get() ? "+" : "-", value.toString()));
-			}
-
-			@Override
-			public CardView.ActiveSorting read(JsonReader in) throws IOException {
-				String val;
-				switch (in.peek()) {
-					case STRING:
-						val = in.nextString();
-						break;
-					case NAME:
-						val = in.nextName();
-						break;
-					default:
-						assert false;
-						return null;
-				}
-
-				boolean descending = val.startsWith("+");
-				CardView.Sorting sorting = CardView.SORTINGS.stream()
-						.filter(s -> val.substring(1).equals(s.toString()))
+		return new StringTypeAdapter<>(
+				v -> String.format("%s%s", v.descending.get() ? "+" : "-", v.toString()),
+				s -> CardView.SORTINGS.stream()
+						.filter(n -> s.substring(1).equals(n.toString()))
 						.findAny()
-						.orElse(null);
-
-				if (sorting == null) {
-					return null;
-				} else {
-					return new CardView.ActiveSorting(sorting, descending);
-				}
-			}
-		};
+						.map(sorting -> new CardView.ActiveSorting(sorting, s.startsWith("+")))
+						.orElse(null)
+		);
 	}
 
 	public static TypeAdapterFactory createPropertyTypeAdapterFactory() {
