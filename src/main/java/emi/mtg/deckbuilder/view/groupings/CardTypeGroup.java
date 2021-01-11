bcc0b484ce6e2ded86a87d540be4b7c755da277b
Emi <dealuminis@gmail.com>
2021-01-10 17:30:54 -0800

    Card type grouping now includes 'unknown'
    Thanks a lot, Scryfall. "Creatue" my foot. You broke my deckbuilder with
    the power of uwu!
    
    Signed-off-by: Emi <dealuminis@gmail.com>

--- src/main/java/emi/mtg/deckbuilder/view/groupings/CardTypeGroup.java
+++ src/main/java/emi/mtg/deckbuilder/view/groupings/CardTypeGroup.java
@@ -5,6 +5,7 @@ import emi.mtg.deckbuilder.model.CardInstance;
 import emi.mtg.deckbuilder.view.components.CardView;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -20,7 +21,9 @@ public class CardTypeGroup extends CharacteristicGrouping implements CardView.Gr
 
 	@Override
 	public String[] groupValues() {
-		return Arrays.stream(CardType.values()).map(CardType::name).toArray(String[]::new);
+		List<String> types = Arrays.stream(CardType.values()).map(CardType::name).collect(Collectors.toList());;
+		types.add("Unknown");
+		return types.toArray(new String[0]);
 	}
 
 	@Override
@@ -35,7 +38,11 @@ public class CardTypeGroup extends CharacteristicGrouping implements CardView.Gr
 				.flatMap(f -> f.type().cardTypes().stream())
 				.collect(Collectors.toSet());
 
-		assert type.size() == 0 : "Disambiguate: " + type.stream().map(CardType::name).collect(Collectors.joining(", "));
+		if (type.isEmpty()) {
+			return "Unknown";
+		}
+
+		assert type.size() == 1 : "Disambiguate: " + type.stream().map(CardType::name).collect(Collectors.joining(", "));
 
 		return type.iterator().next().name();
 	}
