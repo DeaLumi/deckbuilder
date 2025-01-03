f46d35b4e1fd41d24f7f6bf672922020e17bc2a8
Emi <dealuminis@gmail.com>
2025-01-03 14:45:01 -0800

    Slog: Atomize loggings and add throwable error log
    Just some stuff that was mising from the initial slogger implementation.
    
    Signed-off-by: Emi <dealuminis@gmail.com>

--- src/main/java/emi/mtg/deckbuilder/util/Slog.java
+++ src/main/java/emi/mtg/deckbuilder/util/Slog.java
@@ -1,27 +1,40 @@
 package emi.mtg.deckbuilder.util;
 
 import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.util.Formatter;
 
 public class Slog {
 	private final Slog parent;
 
 	private String name;
 	private long stopwatch;
+	private final ThreadLocal<StringBuilder> buffer;
 
 	public Slog(String name) {
 		this(null, name);
-	}
-
-	public Slog rename(String name) {
-		this.name = name;
-		return this;
 	}
 
 	protected Slog(Slog parent, String name) {
 		this.parent = parent;
 		this.name = name;
 		this.stopwatch = -1;
+		this.buffer = new ThreadLocal<StringBuilder>() {
+			@Override
+			protected StringBuilder initialValue() {
+				return new StringBuilder(1024);
+			}
+		};
+	}
+
+	public Slog rename(String name) {
+		this.name = name;
+		return this;
 	}
 
 	protected void nameChain(PrintStream dest) {
@@ -30,6 +43,14 @@ public class Slog {
 			dest.print(" / ");
 		}
 		dest.print(name);
+	}
+
+	protected void nameChain(StringBuilder buffer) {
+		if (parent != null) {
+			parent.nameChain(buffer);
+			buffer.append(" / ");
+		}
+		buffer.append(name);
 	}
 
 	public Slog child(String name) {
@@ -59,12 +80,36 @@ public class Slog {
 		return log(System.err, true, format, args);
 	}
 
+	public Slog err(String format, Throwable t, Object... args) {
+		PrintWriter writer = new PrintWriter(new StringWriter(1024));
+		t.printStackTrace(writer);
+		return log(System.err, true, format + "%n%n%s", t.getMessage(), writer.toString()); // TODO string concatenation
+	}
+
+	private final static DateTimeFormatter TIMESTAMP_FORMAT = new DateTimeFormatterBuilder()
+			.appendLiteral('[')
+			.appendValue(ChronoField.YEAR).appendLiteral('-').appendValue(ChronoField.MONTH_OF_YEAR).appendLiteral('-').appendValue(ChronoField.DAY_OF_MONTH)
+			.appendLiteral(' ')
+			.appendValue(ChronoField.HOUR_OF_DAY).appendLiteral(':').appendValue(ChronoField.MINUTE_OF_HOUR).appendLiteral(':').appendValue(ChronoField.SECOND_OF_MINUTE).appendLiteral('.').appendValue(ChronoField.MILLI_OF_SECOND)
+			.appendLiteral(' ')
+			.appendZoneOrOffsetId()
+			.appendLiteral(']')
+			.toFormatter();
+
 	public Slog log(java.io.PrintStream dest, boolean newline, String format, Object... args) {
-		dest.printf("[%1$tF %1$tT.%1$tL %1$tZ] [", OffsetDateTime.now());
-		nameChain(dest);
-		dest.print("] ");
-		dest.printf(format, args);
-		if (newline) dest.println();
+		StringBuilder sb = buffer.get();
+		sb.setLength(0);
+		sb.append(TIMESTAMP_FORMAT.format(OffsetDateTime.now())).append(" [");
+		nameChain(sb);
+		sb.append("] ");
+		sb.append(format);
+		final String totalFormat = sb.toString();
+
+		synchronized(dest) {
+			dest.printf(totalFormat, args);
+			if (newline) dest.println();
+		}
+
 		return this;
 	}
 }
